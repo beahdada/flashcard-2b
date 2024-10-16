95845d0e383377e4f3c6de8294be1cffee9121f1
beahdada <163007582+beahdada@users.noreply.github.com>
2024-10-16 11:46:06 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Vasco',
+    'Quem fundo o Vasco?',
+    'Mateus Ferreira Biazzeto',
+)
+
+criaCartao(
+    'Batata',
+    'Quem plantou batata em contenda?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido por ser o maior do sul?',
+    'athletico pr'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o EUA?',
+    'Prof marcio'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem construiu o estádio do athletico?',
+    'Conserta Felix'
+)
